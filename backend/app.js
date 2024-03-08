da3fe5b53ccbd5bb5d5e0306282aa057c7b999c3
Sandra925 <sandra.tarutyte925@gmail.com>
2024-03-08 20:13:30 +0200

    Added Item Placeholders

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,23 @@
 const express = require("express");
-
+const path = require('path');
+const fs = require('fs');
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.json("Hello World");
 });
-
+app.get("/items", (req, res) => {
+  const filePath = path.join(__dirname, 'items-placeholder.json');
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading the JSON file');
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
+});
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
