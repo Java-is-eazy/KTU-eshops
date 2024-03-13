fb93aa25ef8c4967a0097a24364bb4f930b99771
Sandra925 <sandra.tarutyte925@gmail.com>
2024-03-13 22:46:12 +0200

    Added .env

--- backend/database.js
+++ backend/database.js
@@ -1,10 +1,18 @@
+require('dotenv').config();
 const mysql = require('mysql');
+
+const dbUsername = process.env.user;
+const dbPassword = process.env.password;
+const dbHost = process.env.host;
+const dbport = process.env.port;
+const dbName = process.env.database;
+
 const connection = mysql.createConnection({
-    host: 'db4free.net',
-    port: '3306',
-    user: 'ktueshopdb',
-    password: 'DBpassword',
-    database: 'ktueshopdb1'
+   user: dbUsername,
+   password: dbPassword,
+   host: dbHost,
+   port: dbport,
+   database: dbName
 })
 
 connection.connect();
