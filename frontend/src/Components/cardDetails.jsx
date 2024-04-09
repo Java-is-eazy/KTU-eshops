c126b40aab889f2ac302e516f4eab0e5db0bcc6b
Sandra925 <sandra.tarutyte925@gmail.com>
2024-04-09 22:51:30 +0300

    Add the result of payments success

--- frontend/src/Components/cardDetails.jsx
+++ frontend/src/Components/cardDetails.jsx
@@ -3,7 +3,9 @@ import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import { useLocation,useParams} from 'react-router-dom';
 import './cardDetailsPage.css';
 import axios from 'axios';
+
 const CardDetailsForm = () => {
+  const [paymentSuccessful, setPaymentSuccessful] = useState(false);
   const stripe = useStripe();
   const elements = useElements();
   const [error, setError] = useState(null);
@@ -34,18 +36,25 @@ const CardDetailsForm = () => {
       try {
         const response = await axios.post(
           'http://localhost:3001/api/checkout/create-payment-intent',
-          {paymentMethod : paymentMethod, productId:productId}
+          { paymentMethod: paymentMethod, productId: productId }
         );
-  
-        const {error, paymentIntent} = await stripe.confirmCardPayment(response.data.clientSecret, {
+    
+        const { error, paymentIntent } = await stripe.confirmCardPayment(response.data.clientSecret, {
           payment_method: {
             card: cardElement,
-            billing_details: {
-            },
+            billing_details: {},
           },
         });
+    
+        if (error) {
+          console.error('Error creating payment intent:', error);
+          setError(error.message); // Set error message again in case of confirmation error
+        } else {
+          setPaymentSuccessful(true); // Set success state if payment confirmed
+        }
       } catch (error) {
         console.error('Error creating payment intent:', error);
+        setError(error.message);
       }
     }
   };
@@ -65,7 +74,7 @@ const CardDetailsForm = () => {
           />
         </label>
         {error && <div style={{ color: 'red' }}>{error}</div>}
-
+        {paymentSuccessful && <div style={{ color: 'green' }}>Payment Successful!</div>}
         <button type="submit">Submit Payment</button>
       </form>
     </div>
